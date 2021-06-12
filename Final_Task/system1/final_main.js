6db77a6e06648d777b3a034bc3b457a3db8da78f
KuandQ <81697090+KuandQ@users.noreply.github.com>
2021-06-12 19:40:13 +0900

    the last submit

--- Final_Task/system1/final_main.js
+++ Final_Task/system1/final_main.js
@@ -1,6 +1,7 @@
 let input_data;
 let bubble_chart;
 let scatter_plot;
+let lineChart;
 let bar_chart;
 let filter = [];
 
@@ -10,41 +11,35 @@ d3.csv("https://kuandq.github.io/InfoVis2021/Final_Task/data.csv")
         input_data.forEach( d => {
             d.Potential = +d.Potential;
             d.Age = +d.Age;
+            d.HeadingAccuracy = +d.HeadingAccuracy;
         });
 
-        scatter_plot = new ScatterPlot( {
-            parent: '#drawing_region_scatterplot',
+        const color_scale = d3.scaleOrdinal( d3.schemeCategory10 );
+        color_scale.domain(['L. Messi','Cristiano Ronaldo','Neymar Jr','De Gea','K. De Bruyne','E. Hazard','L. Modri','L. Surez','Sergio Ramos','J. Oblak','R. Lewandowski','T. Kroos','D. Godn','David Silva','N. Kant','P. Dybala','H. Kane','A. Griezmann','M. ter Stegen','T. Courtois','Sergio Busquets','E. Cavani','M. Neuer','S. Agero','G. Chiellini','K. Mbapp','M. Salah','Casemiro','J. Rodrguez','L. Insigne','Isco','C. Eriksen','Coutinho','P. Aubameyang','M. Hummels','Marcelo','G. Bale','H. Lloris','G. Higua']);
+
+        bubble_chart = new BubbleChart( {
+            parent: '#drawing_region_bubblechart',
             width: 512,
             height: 512,
-            margin: {top:50, right:60, bottom:50, left:100},
-        }, input_data );
-        scatter_plot.update();
-
-       d3.select('#reverse')
-            .on('click', d => {
-            scatter_plot.data.reverse();
-            scatter_plot.update();
-            });
-
-        d3.select('#ascend')
-            .on('click', d => {
-            scatter_plot.data.sort((a,b)=>a.Potential-b.Potential);
-            scatter_plot.update();
-            });
-
-
-
-        scatter_plot1 = new ScatterPlot1( {
-            parent: '#drawing_region_scatterplot1',
-            width: 512,
-            height: 512,
-            margin: {top:50, right:60, bottom:50, left:100},
+            margin: {top:10, right:10, bottom:50, left:50},
             xlabel: 'Age',
-            ylabel: 'Potential [C]',
+            ylabel: 'HeadingAccuracy [C]',
+            cscale: color_scale
         }, input_data );
-        scatter_plot1.update();
+        bubble_chart.update();
 
-       lineChart = new LineChart({
+        bar_chart = new BarChart( {
+            parent: '#drawing_region_barchart',
+            width: 512,
+            height: 512,
+            margin: {top:10, right:10, bottom:50, left:90},
+            xlabel: 'Name',
+            ylabel: 'classification',
+            cscale: color_scale
+        }, input_data );
+        bar_chart.update();
+
+      /* lineChart = new LineChart({
             parent: '#drawing_region_linechart',
             width: 1000,
             height: 256,
@@ -53,6 +48,7 @@ d3.csv("https://kuandq.github.io/InfoVis2021/Final_Task/data.csv")
             yticks: 5,
           },input_data );
           lineChart.update();
+          */
         })
             .catch( error => {
                 console.log( error );
@@ -61,12 +57,12 @@ d3.csv("https://kuandq.github.io/InfoVis2021/Final_Task/data.csv")
 
 
 
-/*function Filter() {
+function Filter() {
     if ( filter.length == 0 ) {
         bubble_chart.data = input_data;
     }
     else {
-        bubble_chart.data = input_data.filter( d => filter.includes( d.Age ) );
+        bubble_chart.data = input_data.filter( d => filter.includes( d.Name ) );
     }
-    scatter_plot.update();
-}*/
+    bubble_chart.update();
+}
--- Final_Task/system2/final_main.js
+++ Final_Task/system2/final_main.js
@@ -1,7 +1,6 @@
 let input_data;
 let bubble_chart;
 let scatter_plot;
-let lineChart;
 let bar_chart;
 let filter = [];
 
@@ -11,35 +10,41 @@ d3.csv("https://kuandq.github.io/InfoVis2021/Final_Task/data.csv")
         input_data.forEach( d => {
             d.Potential = +d.Potential;
             d.Age = +d.Age;
-            d.HeadingAccuracy = +d.HeadingAccuracy;
         });
 
-        const color_scale = d3.scaleOrdinal( d3.schemeCategory10 );
-        color_scale.domain(['L. Messi','Cristiano Ronaldo','Neymar Jr','De Gea','K. De Bruyne','E. Hazard','L. Modri','L. Surez','Sergio Ramos','J. Oblak','R. Lewandowski','T. Kroos','D. Godn','David Silva','N. Kant','P. Dybala','H. Kane','A. Griezmann','M. ter Stegen','T. Courtois','Sergio Busquets','E. Cavani','M. Neuer','S. Agero','G. Chiellini','K. Mbapp','M. Salah','Casemiro','J. Rodrguez','L. Insigne','Isco','C. Eriksen','Coutinho','P. Aubameyang','M. Hummels','Marcelo','G. Bale','H. Lloris','G. Higua']);
-
-        bubble_chart = new BubbleChart( {
-            parent: '#drawing_region_bubblechart',
+        scatter_plot = new ScatterPlot( {
+            parent: '#drawing_region_scatterplot',
             width: 512,
             height: 512,
-            margin: {top:10, right:10, bottom:50, left:50},
+            margin: {top:50, right:60, bottom:50, left:100},
+        }, input_data );
+        scatter_plot.update();
+
+       d3.select('#reverse')
+            .on('click', d => {
+            scatter_plot.data.reverse();
+            scatter_plot.update();
+            });
+
+        d3.select('#ascend')
+            .on('click', d => {
+            scatter_plot.data.sort((a,b)=>a.Potential-b.Potential);
+            scatter_plot.update();
+            });
+
+
+
+        scatter_plot1 = new ScatterPlot1( {
+            parent: '#drawing_region_scatterplot1',
+            width: 512,
+            height: 512,
+            margin: {top:50, right:60, bottom:50, left:100},
             xlabel: 'Age',
-            ylabel: 'HeadingAccuracy [C]',
-            cscale: color_scale
+            ylabel: 'Potential [C]',
         }, input_data );
-        bubble_chart.update();
+        scatter_plot1.update();
 
-        bar_chart = new BarChart( {
-            parent: '#drawing_region_barchart',
-            width: 512,
-            height: 512,
-            margin: {top:10, right:10, bottom:50, left:90},
-            xlabel: 'Name',
-            ylabel: 'classification',
-            cscale: color_scale
-        }, input_data );
-        bar_chart.update();
-
-      /* lineChart = new LineChart({
+       lineChart = new LineChart({
             parent: '#drawing_region_linechart',
             width: 1000,
             height: 256,
@@ -48,7 +53,6 @@ d3.csv("https://kuandq.github.io/InfoVis2021/Final_Task/data.csv")
             yticks: 5,
           },input_data );
           lineChart.update();
-          */
         })
             .catch( error => {
                 console.log( error );
@@ -57,12 +61,12 @@ d3.csv("https://kuandq.github.io/InfoVis2021/Final_Task/data.csv")
 
 
 
-function Filter() {
+/*function Filter() {
     if ( filter.length == 0 ) {
         bubble_chart.data = input_data;
     }
     else {
-        bubble_chart.data = input_data.filter( d => filter.includes( d.Name ) );
+        bubble_chart.data = input_data.filter( d => filter.includes( d.Age ) );
     }
-    bubble_chart.update();
-}
+    scatter_plot.update();
+}*/
